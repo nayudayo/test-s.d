57a609a475968e5b961a22fa3777a8b54f9b6ae9
nayudayo <66724861+nayudayo@users.noreply.github.com>
2023-05-06 20:29:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# test-s.d
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/xunnac)
---
+++ src/app/products.ts
@@ -0,0 +1,34 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Chicken',
+    price: 109,
+    description: 'Inihaw na Chicken'
+  },
+  {
+    id: 2,
+    name: 'Liempo',
+    price: 109,
+    description: 'Inihaw na Liempo'
+  },
+  {
+    id: 3,
+    name: 'Bangus',
+    price: 99,
+    description: 'Inihaw na Bangus'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
